bbeacd54ef11cae05f2c117ef1fc5f6fb3bf7c3d
kodaishiotsuki <0711dr5@gmail.com>
2022-06-15 18:15:57 +0900

    restAPI sectionテーブル insert,getall

---
+++ src/app/Http/Controllers/Api/SectionController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Section;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class SectionController extends Controller
+{
+    public function sectionIndex()
+    {
+        $section = Section::latest()->get();
+        return response()->json($section);
+    }
+
+
+    public function sectionStore(Request $request)
+    {
+        //バリデーション
+        $validateDate = $request->validate([
+            'section_name' => 'required |unique:sections| max:25',
+        ]);
+
+        //insert
+        Section::insert([
+            'class_id' => $request->class_id,
+            'section_name' => $request->section_name,
+            'created_at' => Carbon::now(),
+        ]);
+        return response('Student Section Inserted Successfully');
+    }
+
+
+    public function sectionEdit($id)
+    {
+        $section = Section::findOrFail($id); //一致するidが見つからない場合はエラーを返す
+        return response()->json($section);
+    }
+
+    public function sectionUpdate(Request $request, $id)
+    {
+        //update
+        Section::findOrFail($id)->update([
+            'class_id' => $request->class_id,
+            'section_name' => $request->section_name,
+            'updated_at' => Carbon::now(),
+        ]);
+        return response('Student Section Updated Successfully');
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SclassController;
+use App\Http\Controllers\Api\SectionController;
 use App\Http\Controllers\Api\SubjectController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -22,3 +23,10 @@ Route::post('/subject/store', [SubjectController::class, 'Store']);
 Route::get('/subject/edit/{id}', [SubjectController::class, 'subEdit']);
 Route::post('/subject/update/{id}', [SubjectController::class, 'Update']);
 Route::get('/subject/delete/{id}', [SubjectController::class, 'Delete']);
+
+//Section Class Routes
+Route::get('/section', [SectionController::class, 'sectionIndex']);
+Route::post('/section/store', [SectionController::class, 'sectionStore']);
+Route::get('/section/edit/{id}', [SectionController::class, 'sectionEdit']);
+Route::post('/section/update/{id}', [SectionController::class, 'sectionUpdate']);
+Route::get('/section/delete/{id}', [SectionController::class, 'Delete']);
